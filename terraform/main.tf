e0e8fb44034aa02b58b37bdb95366611ba4ca741
Jitendra SIngh <jitendra.singh2@dkb.de>
2025-06-23 11:38:47 +0200

    Add Taskfile for automated deployment and Task usage

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,25 +21,10 @@ provider "helm" {
   }
 }
 
+# Your Helm chart for s3www + MinIO
 resource "helm_release" "s3www" {
   name      = "s3www"
   chart     = "../charts/s3www"
   namespace = "default"
   values    = [file("../charts/s3www/values.yaml")]
-
-  depends_on = [helm_release.sealed_secrets]
-}
-
-resource "helm_release" "sealed_secrets" {
-  name      = "sealed-secrets"
-  namespace = "kube-system"
-
-  repository = "https://bitnami-labs.github.io/sealed-secrets"
-  chart      = "sealed-secrets"
-  version    = "2.15.3" # Latest stable version as of June 2025
-
-  create_namespace = false
-
-  # Optional: wait for controller to be ready
-  wait = true
 }
